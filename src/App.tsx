f15e5fcfdd63955591e5d2e7da2e0ec1d52fd579
nicolabba <nicola.abbagnato@gmail.com>
2021-12-12 22:38:40 +0100

    Writing multiple texts one after the others

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,76 @@
 import './components/TypedText/TypedText'
 import './App.css';
 import TypedText from './components/TypedText/TypedText'
+import { Input, Modal, ThemeProvider } from '@react95/core';
+import React, { CSSProperties } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <TypedText
-        text="Hello, my name is Nicola Abbagnato."
-        ></TypedText>
-    </div>
-  );
+type TextData = {text: String, onFinishedWriting?: () => void, style?: CSSProperties}
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class App extends React.Component<any, {shownTexts: TextData[], nextText: number}> {
+
+  constructor(props: any) {
+    super(props)
+    this.state = {shownTexts: [], nextText: 0}
+    this.loadTypeNextText = this.loadTypeNextText.bind(this);
+
+  }
+
+  textsToType: TextData[] = [
+    {text: "Hello, my name is Nicola Abbagnato.", onFinishedWriting: () => {sleep(1000).then(() => {this.loadTypeNextText()})}},
+    {text: "How are you?", onFinishedWriting: () => {sleep(3000).then(() => {this.loadTypeNextText()})}},
+    {text: "Wait...", style: {display: 'inline-block'}, onFinishedWriting: () => {sleep(1000).then(() => {this.loadTypeNextText()})}},
+    {text: "Can't really answer, can you?", style: {marginLeft: '1ch', display: 'inline-block'}}
+  ]
+  handleCloseModal = (event: React.MouseEvent) => { console.log(event) }
+  handleButtonClick = (event: React.MouseEvent) => { console.log(event) }
+
+
+  loadTypeNextText() {
+    this.setState({
+      shownTexts: [...this.state.shownTexts, this.textsToType[this.state.nextText]],
+      nextText: this.state.nextText + 1
+    })
+  }
+
+  componentDidMount() {
+    this.loadTypeNextText()
+  }
+
+  render() {
+    const nextText = this.state.nextText
+    return (
+      <div className="App">
+        {this.state.shownTexts.map((element, index) => {
+          return <TypedText
+            numberKey={index + 1}
+            shownCursorKey={nextText}
+            text={element.text}
+            onFinishedWriting={element.onFinishedWriting}
+            style={element.style}
+          ></TypedText>
+        })}
+        {/* <ThemeProvider>
+          <Modal
+            title="Use this!"
+            defaultPosition={{
+              x: 200,
+              y: 200,
+            }}
+            closeModal={handleCloseModal}
+            buttons={[
+              { value: 'Ok', onClick: handleButtonClick },
+              { value: 'Cancel', onClick: handleButtonClick },
+            ]}
+          >
+            <Input></Input>
+          </Modal>
+        </ThemeProvider> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/TypedText/TypedText.tsx
+++ src/components/TypedText/TypedText.tsx
@@ -1,26 +1,33 @@
 import React, { CSSProperties, AnimationEvent } from 'react';
 import styles from './TypedText.module.css'; // Import css modules stylesheet as styles
 
-type Props = { text: String, style?: CSSProperties, onFinishedWriting?: () => {} }
+type Props = { numberKey: number, shownCursorKey: number, text: String, style?: CSSProperties, onFinishedWriting?: () => void }
 
 class TypedText extends React.Component<Props> {
     width = this.props.text.length
-
-    onAnimationEnd(event: AnimationEvent) {
-        if (event.animationName === styles.typing && !!this.props.onFinishedWriting) {
-            this.props.onFinishedWriting()
-        }
+    
+    constructor(props: Props) {
+        super(props)
+        this.onAnimationEnd = this.onAnimationEnd.bind(this);
     }
+
     render() {
         return <p
             className={styles.typedtext}
             style={{
                 width: this.width + 'ch',
-                animation: `${styles.typing} 2s steps(${this.width}), ${styles.blink} .5s step-end infinite alternate`,
+                animation: `${styles.typing} ${this.width / 13}s steps(${this.width}), ${styles.blink} .5s step-end infinite alternate`,
+                borderRight: this.props.numberKey === this.props.shownCursorKey ? `3px solid` : '0px solid',
                 ...this.props.style
             }}
             onAnimationEnd={this.onAnimationEnd}
         >{this.props.text}</p>;
+    }
+
+    onAnimationEnd(event: AnimationEvent) {
+        if (!!event && event.animationName === styles.typing && !!this.props.onFinishedWriting) {
+            this.props.onFinishedWriting()
+        }
     }
 }
 export default TypedText
